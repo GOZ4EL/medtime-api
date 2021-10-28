cd2c66f9f443c0640b1da29e8eec4275580cc54f
goz4el <hordemzerado@gmail.com>
2021-10-28 01:07:02 -0400

    Adding connection to database

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,10 @@
 require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
+use Src\System\DatabaseConnector;
 
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
+
+$dbConnection = (new DatabaseConnector())->getConnection()
 
