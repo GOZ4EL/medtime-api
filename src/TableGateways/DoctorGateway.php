ce2e23770b48f2439636997dfd7724e9f0a43210
goz4el <hordemzerado@gmail.com>
2021-10-29 22:03:40 -0400

    Escribiendo los metodos mas importantes del Doctor Gateway

--- src/TableGateways/DoctorGateway.php
+++ src/TableGateways/DoctorGateway.php
@@ -10,7 +10,7 @@ class DoctorGateWay {
     $this->db = $db;
   }
 
-  public function findAll(): array
+  public function findAll(): Array|Void
   {
     $statement = "SELECT * FROM Doctor;";
 
@@ -23,21 +23,90 @@ class DoctorGateWay {
     }
   }
 
-  public function find($id): array
+  public function find($ci): Array|Void
   {
     $statement = "
-      SELECT d.*
-      FROM Doctor d
-        JOIN User u
-          ON d.user_id=u.id
-      WHERE u.id = ?;
+      SELECT *
+      FROM Doctor
+      WHERE ci = ?;
     ";
 
     try {
       $statement = $this->db->prepare($statement);
-      $statement->execute(array($id));
+      $statement->execute(array($ci));
       $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
       return $result;
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
+
+  public function insert(Array $input, $user_id): Int|Void
+  {
+    $statement = "
+      INSERT INTO Doctor
+        (ci, , user_id, firstname, lastname, starts_at, ends_at, cost)
+      VALUES
+        (:ci, :user_id, :firstname, :lastname, :starts_at, :ends_at, :cost);
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array(
+        'ci' => $input['ci'],
+        'user_id' => $user_id,
+        'firstname' => $input['firstname'],
+        'lastname' => $input['lastname'],
+        'starts_at' => $input['starts_at'],
+        'ends_at' => $input['ends_at'],
+        'cost' => $input['cost']
+      ));
+      return $statement->rowCount();
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
+
+  public function update($ci, Array $input): Int|Void
+  {
+    $statement = "
+      UPDATE Doctor
+      SET
+        firstname = :firstname,
+        lastname = :lastname,
+        starts_at = :starts_at,
+        ends_at = :ends_at,
+        cost = :cost
+      WHERE ci = :ci;
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array(
+        'ci' => (int) $ci,
+        'firstname' => $input['firstname'] ?? null,
+        'lastname' => $input['lastname'] ?? null,,
+        'starts_at' => $input['starts_at'] ?? null,,
+        'ends_at' => $input['ends_at'] ?? null,,
+        'cost' => $input['cost'] ?? null,
+      ));
+      return $statement->rowCount();
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
+
+  public function delete($user_id)
+  {
+    $statement = "
+      DELETE FROM User
+      WHERE id = :user_id;
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array('user_id' => $user_id));
+      return $statement->rowCount();
     } catch (\PDOException $e) {
       exit($e->getMessage());
     }
