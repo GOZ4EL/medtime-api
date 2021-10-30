e709036d9a9a4e02b92ffbd9adeb691a71f5a39c
goz4el <hordemzerado@gmail.com>
2021-10-29 22:51:56 -0400

    Creando Clase estatica con metodos de utilidad relacionados a algunos HTTP responses

---
+++ src/Utils/Utils.php
@@ -0,0 +1,21 @@
+<?php
+namespace Src\Utils;
+
+class Utils {
+  public static function unprocessableEntityResponse()
+  {
+    $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+    $response['body'] = json_encode([
+      'error' => 'Invalid input'
+    ]);
+    return $response;
+  }
+
+  public static function notFoundResponse()
+  {
+    $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+    $response['body'] = null;
+    return $response;
+  }
+}
+
