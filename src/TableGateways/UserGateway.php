f3e1a1c318344d3aa752116623a5d5e517ade9a6
goz4el <hordemzerado@gmail.com>
2021-11-02 17:45:29 -0400

    Agregando la funcion find al UserGateway

--- src/TableGateways/UserGateway.php
+++ src/TableGateways/UserGateway.php
@@ -9,5 +9,46 @@ class UserGateway {
   {
     $this->db = $db;
   }
+
+  public function find($id): Int|Void
+  {
+    $statement = "
+      SELECT *
+      FROM User
+      WHERE id = ?;
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array($id));
+      $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+      return $result;
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
+
+  public function insert(Array $input): Int|Void
+  {
+    $statement = "
+      INSERT INTO User 
+        (email, role, password)
+      VALUES
+        (:email, :role, :password);
+    ";
+
+    try {
+      $statement = $this->db->prepare($statement);
+      $statement->execute(array(
+        "email" => $input["email"],
+        "role" => $input["role"],
+        "password" => password_hash($input["password"], PASSWORD_BCRYPT),
+      ));
+      $user_id = $this->db->lastInsertId();
+      return $user_id;
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
 }
 
