9d6275baa8835a069735877e763368201aa90153
goz4el <hordemzerado@gmail.com>
2021-11-06 22:09:27 -0400

    Agregando funcion para limpiar la base de datos de prueba

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -1,6 +1,8 @@
 <?php
 namespace Src\Utils;
 
+use \Exception;
+
 class Utils {
   public static function unprocessableEntityResponse()
   {
@@ -16,6 +18,17 @@ class Utils {
     $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
     $response['body'] = null;
     return $response;
+  }
+
+  public static function makeTestDatabaseEmpty(): Void
+  {
+    require dirname(__FILE__) . '/../../bootstrap.php';
+
+    if (getenv('DB_DATABASE') != 'testing_medtime') {
+      throw new Exception("The current database isn't the test one");
+    }
+    $sql = "DELETE FROM User";
+    $db_connection->exec($sql);
   }
 }
 
