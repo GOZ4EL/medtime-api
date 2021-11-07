d1a5fec348ed3ec9e1e3d10ad3ad26f81a012df5
goz4el <hordemzerado@gmail.com>
2021-11-06 22:11:32 -0400

    Agregando metodo para crear data requests falsos de forma automatica

--- tests/DoctorControllerTest.php
+++ tests/DoctorControllerTest.php
@@ -1,13 +1,14 @@
 <?php
+include(dirname(__FILE__) . "/../src/Utils/Utils.php");
 use GuzzleHttp\Client;
+use Src\Utils\Utils;
 
 class DoctorControllerTest extends \PHPUnit\Framework\TestCase
 {
-  public function test_post()
-  {
-    $client = new Client();
 
-    $data = array(
+  public function createDummyDoctor(Array $data = null)
+  {
+    $dummy_doctor = array(
       'email' => 'test1@test.com',
       'role' => 'doctor',
       'password' => 'TestPass123',
@@ -19,6 +20,27 @@ class DoctorControllerTest extends \PHPUnit\Framework\TestCase
       'cost' => '50'
     );
 
+    if(isset($data)) {
+      foreach($dummy_doctor as $key => $value) {
+        if (array_key_exists($key, $data)) {
+          $dummy_doctor[$key] = $data[$key];
+        }
+      }
+    }
+    
+    return $dummy_doctor;
+  }
+
+  public function test_post()
+  {
+    Utils::makeTestDatabaseEmpty();
+
+    $client = new Client();
+    $data = $this->createDummyDoctor(array(
+      'email' => 'hordemzerado@gmail.com',
+      'firstname' => 'loko',
+      'cost' => '999.99'
+    ));
     $response = $client->post('http://127.0.0.1/doctor',['json' => $data]);
 
     $this->assertEquals(201, $response->getStatusCode());
