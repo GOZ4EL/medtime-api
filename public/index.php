9778e80e9ee8004d2fffb8699a1b2022fc98f6b2
goz4el <hordemzerado@gmail.com>
2021-11-07 20:20:50 -0400

    Manejando diversos endpoints. Agregando patient a la lista de endpoints validos

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require "../bootstrap.php";
 use Src\Controller\DoctorController;
+use Src\Controller\PatientController;
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -11,17 +12,30 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-if ($uri[1] !== 'doctor') {
-  header("HTTP/1.1 404 Not Found");
-  exit();
-}
-
-$doctor_ci = null;
-if (isset($uri[2])) {
-  $doctor_ci = $uri[2];
-}
-
+$endpoint = strtolower($uri[1]);
 $request_method = $_SERVER["REQUEST_METHOD"];
 
-$controller = new DoctorController($db_connection, $request_method, $doctor_ci);
+switch ($endpoint) {
+  case 'doctor':
+    $doctor_ci = null;
+    if (isset($uri[2])) {
+      $doctor_ci = $uri[2];
+    }
+    $controller = new DoctorController($db_connection, $request_method, $doctor_ci);
+   break;
+  case 'patient':
+    $patient_ci = null;
+    if (isset($uri[2])) {
+      $patient_ci = $uri[2];
+    }
+    $controller = new PatientController($db_connection, $request_method, $patient_ci);
+    break;
+  default:
+    header("HTTP/1.1 404 Not Found");
+    echo json_encode(array(
+      'error' => 'Invalid path'
+    ));
+    exit();
+}
+
 $controller->processRequest();
