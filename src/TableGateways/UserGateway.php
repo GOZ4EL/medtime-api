39c25906c29944ce07746ecb01eab47089b59f98
goz4el <hordemzerado@gmail.com>
2021-11-19 18:06:22 -0400

    Agregando la creacion del json web token cuando un usuario hace login

--- src/TableGateways/UserGateway.php
+++ src/TableGateways/UserGateway.php
@@ -1,6 +1,9 @@
 <?php
 namespace Src\TableGateways;
 
+use DateTimeImmutable;
+use Firebase\JWT\JWT;
+
 class UserGateway {
 
   private $db = null;
@@ -73,11 +76,13 @@ class UserGateway {
 
       $hashed_pass = $result['password'];
       if (password_verify($input['password'], $hashed_pass)) {
+        $jwt = $this->GenerateJWT($result['role']);
+
         $result = array(
           'id' => $result['id'],
           'email' => $result['email'],
           'role' => $result['role'],
-          'jwt' => 'kjasfjkasfk;jlhfkla;sfkfdak;j'
+          'jwt' => $jwt
         );
       } else {
         $result = array(
@@ -88,6 +93,28 @@ class UserGateway {
     } catch (\PDOException $e) {
       exit($e->getMessage());
     }
+  }
+
+  private function GenerateJWT($role) 
+  {
+    $secret_key  = getenv("JWT_SECRET");
+    $issued_at   = new DateTimeImmutable();
+    $expire      = $issued_at->modify('1 week')->getTimestamp();
+    $server_name = "medtime.com.ve";
+
+    $data = array(
+      'iat' => $issued_at->getTimestamp(),
+      'iss' => $server_name,
+      'nbf' => $issued_at->getTimestamp(),
+      'exp' => $expire,
+      'role' => $role,
+    );
+
+    return JWT::encode(
+      $data,
+      $secret_key,
+      'HS512'
+    );
   }
 }
 
